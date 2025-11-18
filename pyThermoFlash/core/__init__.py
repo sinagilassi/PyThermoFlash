7b7495c90484440a0fbaecf4e29c5d918227d6e8
sina.gilassi@gmail.com <sina.gilassi@gmail.com>
2025-11-18 11:46:20 -0500

    add is_flashable method

--- pyThermoFlash/core/__init__.py
+++ pyThermoFlash/core/__init__.py
@@ -4,6 +4,7 @@ from .main import (
     calc_bubble_point_temperature,
     calc_dew_point_temperature,
     calc_isothermal_flash,
+    is_flashable
 )
 
 __all__ = [
@@ -12,4 +13,5 @@ __all__ = [
     'calc_bubble_point_temperature',
     'calc_dew_point_temperature',
     'calc_isothermal_flash',
+    'is_flashable'
 ]
